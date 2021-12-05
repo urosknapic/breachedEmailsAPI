f42de39baf5a658111387332eadf680e7c83717d
urosknapic <uros.knapic@ceneje.si>
2021-12-05 19:03:59 +0100

    Set redis methods to add and access data in redis

---
+++ BreachedEmailsAPI/BreachedEmailsAPI/Database/RedisCache.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace BreachedEmailsAPI.Database
+{
+  public static class RedisCache
+  {
+    public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+      string key,
+      T data,
+      TimeSpan? expireTime = null,
+      TimeSpan? unusedExpireTime = null)
+    {
+      var options = new DistributedCacheEntryOptions();
+      options.AbsoluteExpirationRelativeToNow = expireTime;
+      options.SlidingExpiration = unusedExpireTime;
+      var json = JsonSerializer.Serialize(data);
+      await cache.SetStringAsync(key, json, options);
+    }
+
+    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string key)
+    {
+      var jsonData = await cache.GetStringAsync(key);
+      if(jsonData == null)
+      {
+        return default(T);
+      }
+      return JsonSerializer.Deserialize<T>(jsonData);
+    }
+  }
+}
