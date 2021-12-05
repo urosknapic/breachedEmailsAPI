1e5ea56690253352dde51341130f07eb846a442b
urosknapic <uros.knapic@ceneje.si>
2021-12-05 21:31:30 +0100

    Write tests with mocked data

--- BreachedEmailsAPI/BreachedEmailsAPITests/EmailStorageTests.cs
+++ BreachedEmailsAPI/BreachedEmailsAPITests/EmailStorageTests.cs
@@ -1,8 +1,6 @@
 using Autofac.Extras.Moq;
 using BreachedEmailsAPI.Interfaces;
 using BreachedEmailsAPI.Services;
-using Microsoft.Extensions.Caching.Distributed;
-using Moq;
 using NUnit.Framework;
 
 namespace BreachedEmailsAPITests
@@ -47,109 +45,87 @@ namespace BreachedEmailsAPITests
           .Returns(false);
 
         var service = mock.Create<IEmailStorageService>();
+
         Assert.IsFalse(service.AddEmail(email));
       }
     }
 
-    //[Test]
-    //public void WhenAddEmailAndGetEmail_ReturnEmail()
-    //{
-    //  //var service = new EmailStorageService();
-    //  var email = "something@gmail.com";
+    [Test]
+    public void WhenDeleteEmailThatDoesNotExist_ReturnFalse()
+    {
+      using (var mock = AutoMock.GetLoose())
+      {
+        var email = "something@gmail.com";
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.DeleteEmail(email))
+          .Returns(false);
 
-    //  var addReuslt = _emailService.AddEmail(email);
-    //  //var countEmailsResult = _email_emailService.GetEmailsCount();
-    //  var result = _emailService.GetEmail(email);
+        var service = mock.Create<IEmailStorageService>();
 
-    //  Assert.IsTrue(addReuslt);
-    //  //Assert.AreEqual(countEmailsResult, 1);
-    //  Assert.AreEqual(result, email);
-    //}
+        Assert.IsFalse(service.DeleteEmail(email));
+      }
+    }
 
-    //[Test]
-    //public void WhenAddOneEmail_GetCount1()
-    //{
-    //  //var service = new EmailStorageService();
-    //  var email = "something@gmail.com";
+    [Test]
+    public void WhenDeleteEmailAfterInsert_ReturnTrue()
+    {
+      using (var mock = AutoMock.GetLoose())
+      {
+        var email = "something@gmail.com";
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.DeleteEmail(email))
+          .Returns(false);
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.AddEmail(email))
+          .Returns(true);
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.DeleteEmail(email))
+          .Returns(true);
 
-    //  var addReuslt = _emailService.AddEmail(email);
-    //  //var resultCount = _emailService.GetEmailsCount();
+        var service = mock.Create<IEmailStorageService>();
 
-    //  Assert.IsTrue(addReuslt);
-    //  //Assert.AreEqual(resultCount, 1);
-    //}
-    //[Test]
-    //public void WhenAddTwoEmails_GetCount2()
-    //{
-    //  //var service = new EmailStorageService();
-    //  var emailOne = "something@gmail.com";
-    //  var emailTwo = "somethingForSure@gmail.com";
+        Assert.IsTrue(service.DeleteEmail(email));
+      }
+    }
 
-    //  var firstEmailResult = _emailService.AddEmail(emailOne);
-    //  var secondEmailResult = _emailService.AddEmail(emailTwo);
+    [Test]
+    public void WhenGetNotExistingEmail_ReturnEmptyString()
+    {
+      using (var mock = AutoMock.GetLoose())
+      {
+        var email = "something@gmail.com";
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.GetEmail(email))
+          .Returns(string.Empty);
 
-    //  //var resultCount = _emailService.GetEmailsCount();
+        var service = mock.Create<IEmailStorageService>();
 
-    //  Assert.IsTrue(firstEmailResult);
-    //  Assert.IsTrue(secondEmailResult);
-    //  //Assert.AreEqual(resultCount, 2);
-    //}
+        var result = service.GetEmail(email);
 
-    //[Test]
-    //public void WhenAddTwoIdenticalEmails_GetTrueFirstAndFalseSecond()
-    //{
-    //  var email = "something@gmail.com";
+        Assert.AreEqual(result, string.Empty);
+      }
+    }
 
-    //  var firstEmailResult = _emailService.AddEmail(email);
-    //  var secondEmailResult = _emailService.AddEmail(email);
+    [Test]
+    public void WhenGetEmailAfterInsert_ReturnEmail()
+    {
+      using (var mock = AutoMock.GetLoose())
+      {
+        var email = "something@gmail.com";
 
-    //  //var resultCount = _emailService.GetEmailsCount();
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.AddEmail(email))
+          .Returns(true);
+        mock.Mock<IEmailStorageService>()
+          .Setup(x => x.GetEmail(email))
+          .Returns(email);
 
-    //  Assert.IsTrue(firstEmailResult);
-    //  Assert.IsFalse(secondEmailResult);
-    //  //Assert.AreEqual(resultCount, 1);
-    //}
+        var service = mock.Create<IEmailStorageService>();
 
-    //[Test]
-    //public void WhenDeleteEmailFromEmptyStorage_ReturnFalse()
-    //{
-    //  var email = "something@gmail.com";
+        var result = service.GetEmail(email);
 
-    //  var result = _emailService.DeleteEmail(email);
-
-    //  Assert.IsFalse(result);
-    //}
-    //[Test]
-    //public void WhenDeleteEmailAfterInserting_ReturnTrue()
-    //{
-    //  var email = "something@gmail.com";
-
-    //  var addResult = _emailService.AddEmail(email);
-    //  var deleteResult = _emailService.DeleteEmail(email);
-
-    //  Assert.IsTrue(addResult);
-    //  Assert.IsTrue(deleteResult);
-    //}
-
-    //[Test]
-    //public void WhenDeleteEmailAfterInsert_GetEmptyStringForGettingEmail()
-    //{
-    //  var email = "something@gmail.com";
-
-    //  _emailService.AddEmail(email);
-    //  _emailService.DeleteEmail(email);
-    //  var getEmail = _emailService.GetEmail(email);
-
-    //  Assert.AreEqual(getEmail, string.Empty);
-    //}
-
-    //[Test]
-    //public void WhenAddIncorectEmailFormat_ThrowExceptionForIncorectInput()
-    //{
-    //  var email = "thisisincorectemail";
-
-    //  Assert.Throws<System.FormatException>(() => _emailService.AddEmail(email));
-    //}
-
+        Assert.AreEqual(result, "something@gmail.com");
+      }
+    }
   }
 }
